38f925d65da51a5087a146f85acbe15c1c4a0af6
Gyorgy Noemi <gynoemi33@gmail.com>
2019-10-07 12:36:00 +0200

    Implement add_pocket()

--- savings.py
+++ savings.py
@@ -1,6 +1,18 @@
 import ui
 
 
+def add_pocket():
+
+    pockets = common.open_file("pockets.csv")
+
+    new_pocket_name = ui.get_input("Pocket's name: ")
+    new_pocket_amount = int(ui.get_input("Pocket's amount: "))
+
+    pockets.append([new_pocket_name, new_pocket_amount])
+
+    common.write_to_file(pockets)
+
+
 def savings_options(chosen_option):
 
     while True:
@@ -21,5 +33,5 @@ def start_savings():
     options = ["Show all savings", "Add new pocket", "Remove pocket", "Modify pocket"]
     ui.show_menu(options, "Back to main menu")
 
-    chosen_option = ui.get_input("Please select an option: ")
+    chosen_option = int(ui.get_input("Please select an option: "))
     savings_options(chosen_option)
