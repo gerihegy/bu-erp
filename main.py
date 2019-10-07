4b5977ab3dbcfed036c9b270d529b7a24a0286b1
Nia <zsofi.deak.nia@gmail.com>
2019-10-07 11:18:37 +0200

    Implement menu

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import ui
+
+
+def handle_menu(chosen_option):
+    while True:
+        if chosen_option == 1:
+            print("modul1")
+        elif chosen_option == 2:
+            print("modul2")
+        elif chosen_option == 3:
+            print("modul3")
+        elif chosen_option == 0:
+            sys.exit(0)
+
+
+def main():
+    ui.show_menu()
+    chosen_option = ui.get_inputs()
+    handle_menu(chosen_option)
+
+
+main()
---
+++ ui.py
@@ -0,0 +1,12 @@
+
+def show_menu():
+    options = ["elem1", "elem2", "elem3"]
+    for index, element in enumerate(options, 1):
+        print(f"({index}) {element}")
+    print("(0) Exit")
+
+
+def get_inputs():
+    label = "Choose option: "
+    chosen_option = int(input(label))
+    return chosen_option
