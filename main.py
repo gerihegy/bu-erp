bb9120dd9fc501b93fcd366a6b17202a6b7c9df2
Nia <zsofi.deak.nia@gmail.com>
2019-10-07 14:53:37 +0200

    Solve issue with menu

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from entries import entries
 import savings
 
 
-def handle_menu(chosen_option):
-    while True:
-        if chosen_option == 1:
-            entries.start_module()
-        elif chosen_option == 2:
-            savings.start_savings()
-        elif chosen_option == 3:
-            print("modul3")
-        elif chosen_option == 0:
-            sys.exit(0)
+def handle_menu():
+    chosen_option = int(ui.get_input("Choose an option: "))
+    if chosen_option == 1:
+        entries.start_module()
+    elif chosen_option == 2:
+        savings.start_savings()
+    elif chosen_option == 3:
+        print("modul3")
+    elif chosen_option == 0:
+        sys.exit(0)
+    else:
+        raise KeyError("No such option")
 
 
 def main():
-    options = ["elem1", "Savings", "elem3"]
-    ui.show_menu(options, "Exit")
-    chosen_option = int(ui.get_input("Choose an option: "))
-    handle_menu(chosen_option)
+    while True:
+        options = ["elem1", "Savings", "elem3"]
+        ui.show_menu(options, "Exit")
+        try:
+            handle_menu()
+        except KeyError as error:
+            ui.print_message(str(error))
 
 
 main()
