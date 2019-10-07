88d8787c69a7969453126a43684cf349f055285b
Hajagos Gergő <hajagos.gergo@gmail.com>
2019-10-07 15:23:50 +0200

    implement entries.csv

--- entries/entries.py
+++ entries/entries.py
@@ -1,3 +1,18 @@
+import ui
+import common
+
+
+
 def start_module():
-    print("Hello Module")
-    input("ENTER")
+    ui.clearscreen()
+    expense_list = common.open_file("entries/entries.csv")
+    cost = ui.get_input("What was your last transaction?")
+    category = ui.get_input("What category of expense was it?")
+    print(f'the cost is {cost}, and the cat is:{category}')
+    expense_list.append([cost, category])
+    common.write_to_file("entries.csv", expense_list)
+
+    input()
+
+
+        
