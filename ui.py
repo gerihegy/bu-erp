750bdfa5053e971207a1cd723d9dd91eb06d5550
Gyorgy Noemi <gynoemi33@gmail.com>
2019-10-07 11:59:43 +0200

    Standardize functions

--- ui.py
+++ ui.py
@@ -1,12 +1,10 @@
 
-def show_menu():
-    options = ["elem1", "elem2", "elem3"]
+def show_menu(options, exiting_message):
     for index, element in enumerate(options, 1):
         print(f"({index}) {element}")
-    print("(0) Exit")
+    print(f"(0) {exiting_message}")
 
 
-def get_inputs():
-    label = "Choose option: "
-    chosen_option = int(input(label))
-    return chosen_option
+def get_inputs(label):
+    input_ = input(label)
+    return input_
