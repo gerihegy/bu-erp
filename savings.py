ce6ce9e67360cebbca4c6db81de933cb62a1b0b8
Gyorgy Noemi <gynoemi33@gmail.com>
2019-10-07 12:16:43 +0200

    Implement start_savings()

---
+++ savings.py
@@ -0,0 +1,25 @@
+import ui
+
+
+def start_savings():
+
+    options = ["Show all savings", "Add new pocket", "Remove pocket", "Modify pocket"]
+    ui.show_menu(options, "Back to main menu")
+
+    chosen_option = ui.get_input("Please select an option: ")
+    savings_options(chosen_option)
+
+
+def savings_options(chosen_option):
+
+    while True:
+        if chosen_option == 1:
+            show_savings()
+        elif chosen_option == 2:
+            add_pocket()
+        elif chosen_option == 3:
+            remove_pocket()
+        elif chosen_option == 4:
+            modify_pocket()
+        elif chosen_option == 0:
+            break
