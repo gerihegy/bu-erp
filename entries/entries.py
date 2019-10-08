b9d39adff29186409e096432817ef0b6915678e7
zsoltion-cc <55063936+zsoltion-cc@users.noreply.github.com>
2019-10-08 10:31:50 +0200

    Fix PEP8 and file RW path

--- entries/entries.py
+++ entries/entries.py
@@ -1,15 +1,16 @@
 import ui
 import common
-import datetime
+
 
 def start_module():
     ui.clearscreen()
     expense_list = []
+    path = "entries/entries.csv"
     date = ui.get_input("Please specify the date:" + "\n")
     cost = ui.get_input("What was your last transaction?" + "\n")
     category = ui.get_input("What category of expense was it?" + "\n")
     expense_list.append([date, cost, category])
     print(f'Date:{date} the cost is {cost}, and the cat is:{category}')
-    common.append_to_file("entries.csv", expense_list)
+    common.append_to_file(path, expense_list)
 
     input("Press any key to continue")
