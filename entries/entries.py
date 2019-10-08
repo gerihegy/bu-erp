9c55f26f17679db1753f91dee0f12fd14a2b096c
Hajagos Gergő <hajagos.gergo@gmail.com>
2019-10-08 11:27:13 +0200

    implement entries menu point

--- entries/entries.py
+++ entries/entries.py
@@ -23,7 +23,7 @@ def expense_or_income():
     path = "entries/entries.csv"
     date = ui.get_input("Please specify the date:" + "\n")
     in_out = ui.get_input("Was it an income or an expense:" + "\n")
-    cost = ui.get_input("How much was your last transaction" + "\n")
+    cost = ui.get_input("Specify the amount" + "\n")
     category = ui.get_input("What category of expense was it?" + "\n")
     expense_list.append([in_out, date, cost, category])
     print(f'Type: {in_out} date:{date} the cost is {cost}, and the category is:{category}')
