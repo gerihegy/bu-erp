9bf7fb3b76a15605c3fe6779cee3f0065ac7c76a
Gyorgy Noemi <gynoemi33@gmail.com>
2019-10-08 12:35:34 +0200

    Fix minor bugs in  the entries

--- entries/entries.py
+++ entries/entries.py
@@ -4,12 +4,12 @@ import common
 
 def start_entry_module():
     while True:
-        options = ["Record daily expense" , "mennyit költöttem  idő alatt?"]
+        options = ["Record daily expense", "mennyit költöttem  idő alatt?"]
         ui.show_menu(options, "Back to main menu")
         try:
             chosen_option = int(ui.get_input("Please select an option: "))
             if chosen_option == 1:
-                expense_or_income()
+                declare_expense_or_income()
             elif chosen_option == 0:
                 break
             else:
@@ -28,14 +28,14 @@ def declare_expense_or_income():
     day = ui.get_input("Please specify the day:" + "\n")
     month = ui.get_input("Please specify the month:" + "\n")
     year = ui.get_input("Please specify the year:" + "\n")
-    date = day + month + year
+    date = ".".join([day, month, year])
 
     in_out = ui.get_input("Was it an income or an expense:" + "\n")
     cost = ui.get_input("Specify the amount" + "\n")
     category = ui.get_input("What category of expense was it?" + "\n")
     expense_list.append([in_out, date, cost, category])
 
-    print(f'Type: {in_out} date:{date} the cost is {cost}, and the category is:{category}')
+    print(f'\nType: {in_out} date: {date} the cost is {cost}, and the category is: {category}')
     common.append_to_file(path, expense_list)
 
-    input("Press any key to continue")
+    input("\nPress any key to continue")
